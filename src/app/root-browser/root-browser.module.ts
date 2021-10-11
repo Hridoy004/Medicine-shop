34be5b6bb6e3f9af83ff06729d0273f6f8b9c00f
Sahojahan Miah Hridoy 191.092 <hridoyahmed2435@gmail.com>
2021-10-12 01:47:07 +0600

    initial commit

---
+++ src/app/root-browser/root-browser.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+
+import { RootBrowserRoutingModule } from './root-browser-routing.module';
+import { RootDefaultComponent } from './components/root-default/root-default.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
+import { Error404Component } from './components/error404/error404.component';
+import {AppGenericsModule} from "../app-generics/app-generics.module";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+
+@NgModule({
+  declarations: [
+    RootDefaultComponent,
+    Error404Component,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RootBrowserRoutingModule,
+    AppGenericsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [
+    RootDefaultComponent
+  ]
+})
+export class RootBrowserModule {
+
+}
