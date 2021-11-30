3f89dc710316caf27ace8f923344634577d7fa70
Sahojahan Miah Hridoy 191.092 <hridoyahmed2435@gmail.com>
2021-11-30 23:55:30 +0600

    user profile

--- src/app/root-browser/guards/authentication.guard.service.ts
+++ src/app/root-browser/guards/authentication.guard.service.ts
@@ -2,22 +2,26 @@ import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTre
 import { Injectable } from "@angular/core";
 import {Observable} from "rxjs";
 import {SessionService} from "../services/session.service";
+import {Location} from "@angular/common";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationGuard implements CanActivate {
 
-  public constructor(private router: Router, private sessionService: SessionService) {
+  public constructor(private router: Router,
+                     private sessionService: SessionService,
+                     private location: Location) {
 
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let path = this.location.path();
 
     let token = this.sessionService.getToken();
     console.log('session token: ', token);
     if(token === null || token === undefined) {
-      this.router.navigate(['/auth/login']);
+      this.router.navigate(['/auth/login'], {queryParams: {redirect: path}});
       return false;
     }
     return true;
